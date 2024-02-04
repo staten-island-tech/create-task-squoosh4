e1c5631ef3cd4e87a0ae826cc239ae89c2415c98
squoosh4 <110200988+squoosh4@users.noreply.github.com>
2024-02-04 14:09:50 -0500

    fwea
    fawe

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
 import { validWords } from "./validWords";
+import { DOMSelectors } from "./domselectors";
 
-maxDex = validWords.length - 1;
-wordToGuess = validWords[Math.random() * maxDex];
+const targetRow = 1
+const targetLetter = 1
+const maxDex = validWords.length;
+wordToGuess = validWords[Math.floor(Math.random() * maxDex)];
 console.log(wordToGuess);
 
+function wordGuessCheckerAlg(targetRow) {
+
+    const correctLetters = Array.from(wordToGuess);
+    const guessedLetters = Array.from(DOMSelectors.input.value)
+    console.log(correctLetters);
+    console.log(guessedLetters);
+
+    for (let letterNum = 0; letterNum < 5; letterNum++) {
+        
+        if (guessedLetters[letterNum].lower === correctLetters[letterNum].lower){
+            DOMSelectors.(`r1l + ${letterNum}`)
+        }
+
+    }
+
+
+
+};
+
+DOMSelectors.submitButton.addEventListener("click", function(event){
+
+    event.preventDefault();
+    wordGuessCheckerAlg()
+
+})
