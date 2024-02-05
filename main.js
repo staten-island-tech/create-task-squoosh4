89b652f5ff28b6511ed520159ebf13fe1e0f68da
squoosh4 <110200988+squoosh4@users.noreply.github.com>
2024-02-04 21:55:36 -0500

    rgsesergreh
    ertshshet

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ import { validWords } from "./validWords";
 import { DOMSelectors } from "./domselectors";
 
 const targetRow = 1
-const targetLetter = 1
+
 const maxDex = validWords.length;
 wordToGuess = validWords[Math.floor(Math.random() * maxDex)];
 console.log(wordToGuess);
 
-function wordGuessCheckerAlg(targetRow) {
+function wordGuessCheckerAlg(rowToTarget) {
 
     const correctLetters = Array.from(wordToGuess);
     const guessedLetters = Array.from(DOMSelectors.input.value)
@@ -17,18 +17,28 @@ function wordGuessCheckerAlg(targetRow) {
     for (let letterNum = 0; letterNum < 5; letterNum++) {
         
         if (guessedLetters[letterNum].lower === correctLetters[letterNum].lower){
-            DOMSelectors.(`r1l + ${letterNum}`)
+
+            DOMSelectors.(`r${rowToTarget}l${letterNum}`).insertAdjacentHTML("beforeend", `<span style="background-color: green;">${guessedLetters[letterNum]}</span>`)
+        
+        } 
+        else if (correctLetters.contains(guessedLetters[letterNum].lower)) {
+
+            DOMSelectors.(`r${rowToTarget}l${letterNum}`).insertAdjacentHTML("beforeend", `<span style="background-color: yellow;">${guessedLetters[letterNum]}</span>`)
+        
+        }
+        else {
+            .insertAdjacentHTML("beforeend", `<span>${guessedLetters[letterNum]}</span>`)
         }
 
     }
 
-
+    rowToTarget++;
 
 };
 
 DOMSelectors.submitButton.addEventListener("click", function(event){
 
-    event.preventDefault();
-    wordGuessCheckerAlg()
+    event.preventDefault;
+    wordGuessCheckerAlg(targetRow);
 
-})
+});
